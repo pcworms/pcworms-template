e5c9f76cf418fd2a238564d6626b1dc5216a2d45
bsimjoo <bsimjoo@gmail.com>
2022-06-19 23:03:01 +0430

    removed log

--- template-parts/part/post-slide.php
+++ template-parts/part/post-slide.php
@@ -2,14 +2,7 @@
 $post = get_post();
 $title = $post->post_title;
 $ID = get_the_ID();
-$content = $post->post_content;
-Console_log([
-    "title"=>$title,
-    "ID"=>$ID,
-    "content"=>$content,
-    "summery"=>$summery
-    ])
-?>
+$content = $post->post_content;?>
 <div class="item<?php echo ($args['counter'] == 0) ? ' active' : ''; ?>">
     <div class="post-slide">
         <a href="#post-<?php echo $ID?>"><h3><?php echo $title;?></h3></a>
