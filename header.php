192271c12e6ff7c96f3eac77a3750ebbdbbf929b
bsimjoo <bsimjoo@gmail.com>
2022-08-25 20:26:04 +0430

    add social media preview

--- header.php
+++ header.php
@@ -8,13 +8,34 @@
  *
  */
 
-?><!DOCTYPE html>
+?>
+
+<?php
+// Social media preview
+$title = is_front_page() ? get_bloginfo( 'name' ) : get_the_title().'|'.get_bloginfo( 'name' );
+$image = "";
+$desc = "";
+if (is_single()){
+	$image = get_the_post_thumbnail_url();
+	$description = get_the_excerpt();
+}
+if ($image==""){
+	$custom_logo_id = get_theme_mod( 'custom_logo' );
+    $image = wp_get_attachment_image_src( $custom_logo_id , 'full' )[0];
+}
+if ($desc=="")
+	$desc = get_bloginfo('description');
+?>
+<!DOCTYPE html>
 <html <?php language_attributes(); ?>>
-<head>
-<meta charset="<?php bloginfo( 'charset' ); ?>">
-<meta http-equiv="X-UA-Compatible" content="IE=edge" />
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="profile" href="http://gmpg.org/xfn/11">
+<head prefix="og: https://ogp.me/ns#">
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" >
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<meta property="og:title" content="<?php echo $title?>">
+    <meta property="og:image" content="<?php echo $image?>">
+	<meta property="og:description" content="<?php echo $desc?>">
 <?php wp_head(); ?>
 </head>
 <body <?php body_class('line-numbers'); ?>>
