174e22bd57c3b7e9b9061a42d51b637933cb15c9
bsimjoo <bsimjoo@gmail.com>
2022-10-19 11:05:09 +0330

    using post summery in slide show

--- template-parts/part/post-slide.php
+++ template-parts/part/post-slide.php
@@ -2,7 +2,13 @@
 $post = get_post();
 $title = $post->post_title;
 $ID = get_the_ID();
-$content = $post->post_content;?>
+// $content = $post->post_content;
+$post_id = get_the_ID();
+$post_field = get_post_field( 'post_content', $post_id );
+$content_parts = get_extended( $post_field );
+$content = $content_parts['main'];
+$content = apply_filters( 'the_content', $content );
+?>
 <div class="item<?php echo ($args['counter'] == 0) ? ' active' : ''; ?>">
     <div class="post-slide">
         <a href="#post-<?php echo $ID?>"><h3><?php echo $title;?></h3></a>
